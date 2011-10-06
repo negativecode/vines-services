a2ff218213a52c54d9c70fc385146b0672baadf5
dgraham <david.malcom.graham@gmail.com>
2011-10-05 20:38:32 -0600

    Fix config file rewrites.

--- lib/vines/services/command/init.rb
+++ lib/vines/services/command/init.rb
@@ -5,75 +5,79 @@ module Vines
     module Command
       class Init
         def run(opts)
-          raise 'vines-services init [domain]' unless opts[:args].size == 1
-          @domain = opts[:args].first.dup
-          dir = File.expand_path(@domain)
-          @component_password = Kit.generate_password
-          create_directories
-          initialize_server
-          initialize_component
-          initialize_agent
-          puts "Initialized service, agent, and server directories: #{@domain}"
+          raise 'vines-services init <domain>' unless opts[:args].size == 1
+          @domain = opts[:args].first.downcase
+          base = File.expand_path(@domain)
+          raise "Directory already initialized: #{@domain}" if File.exists?(base)
+          raise "Agent gem required: gem install vines-agent" unless agent_gem_installed?
+
+          @db = find_db
+          save_user(ask_for_jid, ask_for_password)
+          @token = Kit.generate_password
+
+          Dir.mkdir(base)
+          %w[server services agent].each do |sub|
+            dir = File.expand_path(sub, base)
+            Dir.mkdir(dir)
+            Dir.chdir(dir) { send("init_#{sub}") }
+          end
+          puts "Initialized server, agent, and services directories: #{@domain}"
+          puts "Login at http://localhost:5280/"
         end
 
         private
 
-        def initialize_server
-          Dir.chdir(File.join(@domain, "server"))
+        def init_server
           `vines init #{@domain}`
-          configure_database
-          FileUtils.mv Dir.glob("#{@domain}/*"), "./"
+          FileUtils.mv(Dir.glob("#{@domain}/*"), '.')
           FileUtils.remove_dir(@domain)
+          FileUtils.remove_dir('data/users')
+          web = File.expand_path("../../../../../web", __FILE__)
+          FileUtils.cp_r(Dir.glob("#{web}/*"), 'web')
+          update_server_config('conf/config.rb')
           `vines start -d`
-          FileUtils.cp_r(File.expand_path("../../../../../web/index.html", __FILE__), File.join("web", "index.html"))
-          FileUtils.cp_r(File.expand_path("../../../../../web/javascripts", __FILE__), File.join("web", "javascripts"))
-          FileUtils.cp_r(File.expand_path("../../../../../web/stylesheets", __FILE__), File.join("web", "stylesheets"))
-          FileUtils.cp_r(File.expand_path("../../../../../web/images", __FILE__), File.join("web", "images"))
-          FileUtils.cp_r(File.expand_path("../../../../../web/coffeescripts", __FILE__), File.join("web", "coffeescripts"))
+          puts "Started vines server: vines start -d"
         end
 
-        def initialize_agent
-          Dir.chdir("../agent/")
+        def init_agent
           `vines-agent init #{@domain}`
-          FileUtils.cp("../server/conf/certs/#{@domain}.crt", File.join("#{@domain}", "conf", "certs"))
-          agent_password = Kit.generate_password
-          jid = Vines::JID.new(fqdn, @domain).bare
-          id = "user:#{jid}"
-          Fiber.new do
-            user = Vines::Services::CouchModels::User.new(id: id)
-            user.name = jid
-            user.system = true
-            user.password = agent_password
-            if user.valid?
-              user.save
-              log.debug("Saving user: #{user}")
-            end
-          end.resume
-          update_agent_config(File.join("#{@domain}", "conf", "config.rb"), agent_password)
-          FileUtils.mv Dir.glob("#{@domain}/*"), "./"
+          FileUtils.mv(Dir.glob("#{@domain}/*"), '.')
           FileUtils.remove_dir(@domain)
+          FileUtils.cp("../server/conf/certs/#{@domain}.crt", 'conf/certs')
+          require File.expand_path('conf/config.rb')
+          token = Vines::Agent::Config.instance.domain.password
+          save_user(Vines::JID.new(fqdn, @domain), token, true)
           `vines-agent start -d`
+          puts "Started vines agent: vines-agent start -d"
         end
 
-        # Coordinate all the configuration required for the service
-        def initialize_component
-          Dir.chdir("../services/")
-          %w[conf].each do |sub|
-            FileUtils.cp_r(File.expand_path("../../../../../#{sub}", __FILE__), File.join("."))
-          end
-          log, pid = %w[log pid data data/index].map do |sub|
-            File.join(sub).tap {|subdir| Dir.mkdir(subdir) }
-          end
-          update_config(File.join("conf", "config.rb"))
+        def init_services
+          %w[log pid data data/index data/upload].each {|sub| Dir.mkdir(sub) }
+          %w[data data/index data/upload].each {|dir| File.chmod(0700, dir) }
+          FileUtils.cp_r(File.expand_path("../../../../../conf", __FILE__), '.')
+          File.chmod(0600, 'conf/config.rb')
+          update_services_config('conf/config.rb')
           `vines-services start -d`
+          puts "Started vines services component: vines-services start -d"
         end
 
-        #create all the directories for the server, agent, and service
-        def create_directories
-          Dir.mkdir(@domain)
-          %w[server services agent].each do |sub|
-            Dir.mkdir(File.join(@domain, sub))
+        def agent_gem_installed?
+          require 'vines/agent'
+        rescue LoadError
+          false
+        end
+
+        def find_db
+          host, port = 'localhost', 5984
+          db = @domain.downcase.gsub('.', '_')
+          until create_db(host, port, db)
+            puts "CouchDB connection failed"
+            $stdout.write('CouchDB Host: ')
+            host = $stdin.gets.chomp
+            $stdout.write('CouchDB Port: ')
+            port = $stdin.gets.chomp
           end
+          {host: host, port: port, name: db}
         end
 
         def create_db(host, port, db)
@@ -88,69 +92,42 @@ module Vines
           false
         end
 
-        # This will attempt to create a new couch database. If one exists
-        # by the name generated, it will not be deleted. This will loop until
-        # it gets a valid couch db address and port
-        def configure_database
-          server, port = "127.0.0.1", 5984
-          db_name = @domain.gsub(".", "_").downcase()
-          until create_db(server, port, db_name)
-            puts "Unable to connect."
-            $stdout.write('CouchDB Server: ')
-            server = $stdin.gets.chomp
-            $stdout.write('CouchDB Port: ')
-            port = $stdin.gets.chomp
-          end
-          @couch_server = server
-          @couch_port = port
-          @couch_db = db_name
-          update_config(File.join(@domain, "conf", "config.rb"))
-          @storage = Vines::Services::Storage::CouchDB.new do
-            host server
-            port port
-            database db_name
-            index_dir '.'
-          end
-          create_user
+        def save_user(jid, password, system=false)
+          server = CouchRest::Server.new("http://#{@db[:host]}:#{@db[:port]}")
+          CouchRest::Model::Base.database = server.database(@db[:name])
+          Fiber.new do
+            Vines::Services::CouchModels::User.new.tap do |user|
+              user.id = "user:#{jid}"
+              user.password = password
+              user.system = system
+              user.admin! unless system
+            end.save
+          end.resume
+          puts "\nCreated user: #{jid}"
         end
 
-        #Create a new vines user object in the couch database
-        def create_user
-          jid, password = nil, nil
+        def ask_for_jid
+          jid = nil
           until jid
             $stdout.write('JID: ')
             if node = $stdin.gets.chomp.split('@').first
               jid = Vines::JID.new(node, @domain) rescue nil
             end
           end
+          jid
+        end
 
+        def ask_for_password
+          password = nil
           until password
             $stdout.write('Password: ')
             `stty -echo`
             password = $stdin.gets.chomp
             password = nil if password.empty?
             `stty echo`
+            puts unless password
           end
-          puts "\nCreated #{jid}"
-
-          id = "user:#{jid}"
-          Fiber.new do
-            user = Vines::Services::CouchModels::User.new(id: id)
-            user.name = jid
-            user.password = password
-            if user.valid?
-              user.save
-              log.debug("\nSaving user: #{user}")
-            end
-          end.resume
-        end
-
-        # Set the agents password to the one we generated in this initialization
-        def update_agent_config(config, password)
-          text = File.read(config)
-          File.open(config, 'w') do |f|
-            f.write(text.gsub(/password.*/, "password '#{password}'"))
-          end
+          password
         end
 
         # Return the fully qualified domain name for this machine. This is used
@@ -163,47 +140,40 @@ module Vines
           system.fqdn.downcase
         end
 
-        # Change the config file to contain the values we generated or validated.
-        def update_config(config)
-# FIXME vines.local
-          orig_config = """
-  host 'vines.local' do
-    cross_domain_messages false
-    private_storage false
-    storage 'fs' do
-      dir 'data/users'
-    end
-    # components 'tea'  => 'secr3t',
-    #            'cake' => 'passw0rd'
-  end"""
-          new_config = """
-  host '#{@domain}' do
-    cross_domain_messages false
-    private_storage true
-    storage 'couchdb' do
-      host '#{@couch_server}'
-      port #{@couch_port}
-      database '#{@couch_db}'
-      tls false
-      username ''
-      password ''
-    end
-    components 'vines'  => '#{@component_password}'
-  end"""
+        def update_services_config(config)
           text = File.read(config)
-          text = text.gsub(orig_config, new_config)
-          text = text.gsub("host 'vines.wonderland.lit'", "host 'vines.#{@domain}'")
-          text = text.gsub("secr3t", "#{@component_password}")
-          text = text.gsub("host 'localhost'", "host '#{@couch_server}'")
-          text = text.gsub("port 5984", "port #{@couch_port}")
-          text = text.gsub("database 'xmpp'", "database '#{@couch_db}'")
           File.open(config, 'w') do |f|
-            f.write(text)
+            replaced = text
+              .gsub('wonderland.lit', @domain.downcase)
+              .gsub('secr3t', @token)
+              .gsub("host 'localhost'", "host '#{@db[:host]}'")
+              .gsub("port 5984", "port #{@db[:port]}")
+              .gsub("database 'xmpp'", "database '#{@db[:name]}'")
+            f.write(replaced)
+          end
+        end
+
+        def update_server_config(config)
+          replacement = %Q{
+            storage 'couchdb' do
+              host '#{@db[:host]}'
+              port #{@db[:port]}
+              database '#{@db[:name]}'
+              tls false
+              username ''
+              password ''
+            end
+            components 'vines' => '#{@token}'
+          }
+          text = File.read(config)
+          File.open(config, 'w') do |f|
+            replaced = text
+              .gsub('Vines::Config.configure do', "require 'vines/services/roster'\n\nVines::Config.configure do")
+              .gsub(/\s{4}storage 'fs' do.*\s{4}end/m, replacement)
+            f.write(replaced)
           end
         end
       end
     end
   end
 end
-
-
