fa02d38b141454a2f63f6ff8519cf405607405b2
dgraham <david.malcom.graham@gmail.com>
2011-10-11 20:51:30 -0600

    Add other users to the roster as well as systems.

--- lib/vines/services/roster.rb
+++ lib/vines/services/roster.rb
@@ -10,27 +10,60 @@ module Vines
       def find_user(jid)
         user = _services_find_user(jid)
         return unless user
+        user.roster = []
+
+        systems, users = find_users.partition {|u| u['system'] }
+        systems.map! {|u| u['_id'].sub('user:', '') }
+        return user if systems.include?(user.jid.to_s)
         services = find_services(user.jid)
-        user.roster = services.map do |row|
-          Contact.new(
+
+        users.each do |row|
+          id = row['_id'].sub('user:', '')
+          user.roster << Contact.new(
+            :jid => id,
+            :name => row['name'],
+            :subscription => 'both',
+            :groups => ['People']) unless id == jid.to_s
+        end
+
+        services.each do |row|
+          user.roster << Contact.new(
             :jid => row['jid'],
             :name => row['name'],
             :subscription => 'to',
             :groups => ['Services'])
         end
+
         find_systems(services).each do |name, groups|
+          id = JID.new(name.dup, user.jid.domain).to_s
           user.roster << Contact.new(
-            :jid => JID.new(name.dup, user.jid.domain),
+            :jid => id,
             :name => name,
             :subscription => 'to',
-            :groups => ['Systems', *groups])
+            :groups => ['Systems', *groups]) if systems.include?(id)
         end
+
         user.roster << Contact.new(
           :jid => "vines.#{user.jid.domain}",
           :name => 'Vines',
           :subscription => 'to')
         user
       end
+
+      # Return the User documents for all users and systems.
+      def find_users
+        url = "%s/_design/User/_view/all?reduce=false&include_docs=true" % @url
+        http = EM::HttpRequest.new(url).get
+        http.errback { yield [] }
+        http.callback do
+          doc = if http.response_header.status == 200
+            rows = JSON.parse(http.response)['rows'] rescue []
+            rows.map {|row| row['doc'] }
+          end
+          yield doc || []
+        end
+      end
+      fiber :find_users
 
       # Return the Service documents to which this JID has permission to
       # access.
